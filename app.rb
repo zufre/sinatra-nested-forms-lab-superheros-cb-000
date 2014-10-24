a41995b8e89945cd4708d306274e60aa134fdc43
ahimmelstoss <ahimmelstoss@gmail.com>
2014-10-23 23:39:46 -0400

    change name of post route to match readme

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ class App < Sinatra::Base
     erb :super_hero
   end
 
-  post '/team' do
+  post '/teams' do
     @team_name = params[:team][:name]
     @team_motto = params[:team][:motto]
     @hero_name = []
