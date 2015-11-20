60ca2874d75c91c17a0466cd59eaa0a33a3d046d
vicfriedman <victoria.m.friedman@gmail.com>
2015-11-20 10:22:46 -0500

    tests

--- spec/super_sinatra_forms_spec.rb
+++ spec/super_sinatra_forms_spec.rb
@@ -16,49 +16,33 @@ describe App do
   end
 
   describe 'POST /teams' do
+    it 'submits the form' do
       visit '/'
-      fill_in 
-      post '/teams', {
-        :team => {
-          :name => "Team Ruby",
-          :motto => "We love Ruby!",
-          :members => {
-            :super1 => {
-              :name => "Amanda",
-              :power => "Ruby",
-              :bio => "I love Ruby!"  
-            },
-            :super2 => {
-              :name => "Arel",
-              :power => "JavaScript",
-              :bio => "I love JavaScript!"  
-            },
-            :super3 => {
-              :name => "Katie",
-              :power => "Sinatra",
-              :bio => "I love Sinatra!"  
-            }
-          }
-        }
-      }
-    end
+      fill_in("team[name]", :with => "Team Ruby")
+      fill_in("team[motto]", :with => "We love Ruby!")
+      fill_in("team[members][0][name]", :with => "Amanda")
+      fill_in("team[members][0][power]", :with => "Ruby")
+      fill_in("team[members][0][bio]", :with => "I love Ruby!")
+      fill_in("team[members][1][name]", :with => "Arel")
+      fill_in("team[members][1][power]", :with => "JavaScript")
+      fill_in("team[members][1][bio]", :with => "I love JavaScript!")
+      fill_in("team[members][2][name]", :with => "Katie")
+      fill_in("team[members][2][power]", :with => "Sinatra")
+      fill_in("team[members][2][bio]", :with => "I love Sinatra!")
+      click_button 'submit'
 
-    it 'sends a 200 status code' do 
-      expect(last_response.status).to eq(200)
-    end
-
-    it 'displays the team info name upon submission' do 
-      expect(last_response.body).to include("Team Ruby")
-    end
-
-    it 'displays the team motto upon submission' do 
-      expect(last_response.body).to include("We love Ruby!")
-    end
-
-    it 'shows team member info' do 
-      expect(last_response.body).to include("Amanda")
-      expect(last_response.body).to include("Arel")
-      expect(last_response.body).to include("Katie")
+      expect(page).to have_text("Team Ruby")
+      expect(page).to have_text("Team Motto: We love Ruby!")
+      expect(page).to have_text("Hero Name: Amanda")
+      expect(page).to have_text("Hero power: Ruby")
+      expect(page).to have_text("Hero biography: I love Ruby!")
+      expect(page).to have_text("Hero Name: Arel")
+      expect(page).to have_text("Hero power: JavaScript")
+      expect(page).to have_text("Hero biography: I love JavaScript!")
+      expect(page).to have_text("Hero Name: Katie")
+      expect(page).to have_text("Hero power: Sinatra")
+      expect(page).to have_text("Hero biography: I love Sinatra!")
     end
   end
+  
 end
